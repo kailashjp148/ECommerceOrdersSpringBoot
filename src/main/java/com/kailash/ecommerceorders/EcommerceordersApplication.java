1797d6a08857c27b5ef52b73b25dd8e9640fb2b1
Kailash JP <kailashjp148@gmail.com>
2025-08-08 09:24:48 +0530

    Creating orders spring boot app

---
+++ src/main/java/com/kailash/ecommerceorders/EcommerceordersApplication.java
@@ -0,0 +1,13 @@
+package com.kailash.ecommerceorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kailash/ecommerceorders/EcommerceordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kailash.ecommerceorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
